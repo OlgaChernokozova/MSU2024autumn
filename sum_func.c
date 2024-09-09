666068071eed6a5f85522352713d10079711d52c
PocherevinRV <117889867+PocherevinRV@users.noreply.github.com>
2024-09-09 11:42:29 +0300

    remove mistake in condition for second element

--- sum_func.c
+++ sum_func.c
@@ -4,7 +4,7 @@ double sum_with_condition(FILE *f)
 {
         
 	double sum = 0., current = 0., previous = 0.;
-	if (fscanf(f, "%lf", &current) != 1)
+	if (fscanf(f, "%lf", &previous) != 1)
 	{
 		printf("File is empty");
 		return 0.;
